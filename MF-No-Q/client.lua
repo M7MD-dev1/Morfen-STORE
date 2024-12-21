6d15e1f2812cf90cc1932ab65901de3eb20d7f03
M7MD ! <linsm7md@gmail.com>
2024-12-21 14:26:01 +0300

    Add files via upload

---
+++ MF-No-Q/client.lua
@@ -0,0 +1,6 @@
+CreateThread(function()
+    while true do
+        Wait(0)
+        DisableControlAction(0, 44, false)
+    end
+end)
---
+++ MF-No-Q/fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'M7MD !'
+
+client_scripts {
+    'client.lua',
+}
